0fa584e3efc6173ee02de7fc50fb946b808c2e72
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-07-27 22:46:37 +0300

    Create integer.go

---
+++ data types/integer.go
@@ -0,0 +1,10 @@
+package main
+
+import ("fmt")
+
+func main() {
+var x int = 500
+var y int = -4500
+  fmt.Printf("Veri tipi: %T, değeri: %v", x, x)
+  fmt.Printf("Veri Tipi: %T, değeri: %v", y, y)
+}
