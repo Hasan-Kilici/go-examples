25934275083a92900f813108dd109326bf58451c
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-07-27 22:43:35 +0300

    Create boolean.go

---
+++ data types/boolean.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main() {
+  var b1 bool = true 
+  var b2 = true 
+  var b3 bool 
+  b4 := true 
+
+  fmt.Println(b1) // true ya döner
+  fmt.Println(b2) // true ya döner
+  fmt.Println(b3) // false a döner
+  fmt.Println(b4) // true ya döner
+}
