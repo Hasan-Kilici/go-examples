fff20448ba8ee0e797dfdb0c0eed9b70eea1ede3
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-07-27 22:41:37 +0300

    Create basic.go

---
+++ data types/basic.go
@@ -0,0 +1,14 @@
+package main
+import ("fmt")
+
+func main() {
+  var a bool = true     
+  var b int = 5         
+  var c float32 = 3.14  
+  var d string = "Merhaba Dünya!"  
+
+  fmt.Println("Boolean: ", a)
+  fmt.Println("Integer: ", b)
+  fmt.Println("Float:   ", c)
+  fmt.Println("String:  ", d)
+}
