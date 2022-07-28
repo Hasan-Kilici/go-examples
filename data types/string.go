18ae0ce3066fbd2ee5c8ea47215059ff8195288e
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-07-28 12:36:23 +0300

    Create string.go

---
+++ data types/string.go
@@ -0,0 +1,14 @@
+package main
+import (
+"fmt"
+)
+
+func main() {
+var yazi1 string = "Merhaba Dünya"
+var yazi2 = "Merhaba Dünya"
+yazi3 := "Merhaba Dünya"
+  
+fmt.Print("Veri tipi: %T, değeri: %v\n", yazi1)
+fmt.Print("Veri tipi: %T, değeri: %v\n", yazi2)
+fmt.Print("Veri tipi: %T, değeri: %v\n", yazi3)
+}
