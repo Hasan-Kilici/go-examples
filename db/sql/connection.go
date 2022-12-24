6c4e783a2527a445d73672f75fb4eba2b85cbbba
Hasan Kılıcı <105741983+Hasan-Kilici@users.noreply.github.com>
2022-12-25 00:39:32 +0300

    Create connection.go

---
+++ db/sql/connection.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+ "database/sql"
+)
+
+func main(urunid){
+  db, err := sql.Open("sql", "user:password@/dbname");
+  dbrows, err := trstokdb.Query("SELECT * FROM ogrenci");
+  for stokrows.Next() {
+    var isim string
+    var soyisim string
+    var no int
+    var sinif string
+    
+    err = dbrows.Scan(&isim , &soyisim , &no, &sinif)
+    if err != nil {
+        panic(err.Error())
+    }
+ }
+}
